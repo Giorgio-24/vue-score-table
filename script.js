c7b4539174e837d63492bb670e4db646ba352596
Giorgio Andrei <cipuandrei@libero.it>
2021-08-05 12:01:52 +0200

    player-basic-structure-created

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ var root = new Vue(
     {
         el: '#root',
         data: {
+            players: [
+                { name: 'Giova', score: 0, },
+                { name: 'Simo', score: 20, },
+                { name: 'Daniele', score: 15, },
+
+            ],
+
 
         },
         methods: {
+
+
 
         },
     });
